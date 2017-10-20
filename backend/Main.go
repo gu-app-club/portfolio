ba154a57a0a44dd16b36a53bd0c17e58fa0f2d13
Max Chehab <maxchehab@gmail.com>
2017-10-19 21:03:07 -0700

    Fixed CORS bug by changing X-Requested-With header to content-type

--- backend/Main.go
+++ backend/Main.go
@@ -11,9 +11,9 @@ func main() {
 	router := NewRouter()
 
 	// Fix CORS problems
-	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With"})
+	headersOk := handlers.AllowedHeaders([]string{"content-type"})
 	originsOk := handlers.AllowedOrigins([]string{"*"}) //TODO: Change to be just our front-end domain probably
 	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
 
-	log.Fatal(http.ListenAndServe(":8080", handlers.CORS(originsOk, headersOk, methodsOk)(router)))
+	log.Fatal(http.ListenAndServe(":8080", handlers.CORS(headersOk, originsOk, methodsOk)(router)))
 }
