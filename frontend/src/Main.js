d4b44953c6d624095e87fa92467e4f4f02839109
Max Chehab <maxchehab@gmail.com>
2017-10-21 12:12:45 -0700

    Removed GitHub login link #5

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -21,10 +21,6 @@ class App extends React.Component {
           <Main>
             <ArticleList articles={this.props.articles} />
           </Main>
-
-          <a href="https://github.com/login/oauth/authorize?scope=user:email&client_id=7a32373e9098053826b5">
-            Login
-          </a>
         </AppWrapper>
       </Themed>
     );
