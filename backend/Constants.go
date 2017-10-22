383c54f73c5e1b59f521e934071f6589c9ee37b7
Max Chehab <maxchehab@gmail.com>
2017-10-22 16:49:48 -0700

    Allowed all origins access to api and reverted constants mysql cred.

--- backend/Constants.go
+++ backend/Constants.go
@@ -5,5 +5,5 @@ const SQLPORT = ":3306"
 const SQLURL = BASEURL + SQLPORT
 const TEST_DBNAME = "gu_port_testing"
 const DBNAME = "gu_port_dev"
-const SQLUSER = "gu-port"
-const SQLPASS = "gu-port"
+const SQLUSER = "root"
+const SQLPASS = ""
