de8f59067fcf81f04419e8706e2541deab9b7952
vagrant <vagrant@vagrant.vm>
2017-10-22 20:54:18 +0000

    Add catch; add prod-run

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -6,8 +6,16 @@ import PropTypes from "prop-types";
 
 export default class extends React.Component {
   static async getInitialProps() {
-    const { data } = await api.getPageList(10, 0);
-    return { articles: data.pages };
+    let data;
+    try {
+      data = await api.getPageList(10, 0);
+    } catch (err) {
+      console.error(
+        "Can't connect to the backend :( Are we sure it's running?",
+        err
+      );
+    }
+    return { articles: data.data.pages };
   }
 
   static propTypes = {
