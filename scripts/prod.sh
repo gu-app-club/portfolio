938184f54e5f6c49af8a4d298f61805768d1b4eb
maxchehab <maxchehab@gmail.com>
2017-10-24 03:07:15 +0000

    Updated prod script

--- scripts/prod.sh
+++ scripts/prod.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+export GOPATH=/root/go
+
 # For explaination of how this works, see
 # https://stackoverflow.com/questions/3004811/how-do-you-run-multiple-programs-in-parallel-from-a-bash-script
-
-(cd ~/portfolio/frontend; yarn build; yarn start -p 80) &   # Run frontend  
-(cd ~/portfolio/backend; sh ./build.sh) &&  # Run backend
+echo "MY GO PATH IS: "  $GOPATH
+(cd ~/go/src/github.com/gu-app-club/portfolio/frontend; yarn build; yarn start -p 80) &   # Run frontend  
+(cd ~/go/src/github.com/gu-app-club/portfolio/backend; sh ./build.sh) &&  # Run backend
 fg # Layer in the forground 
