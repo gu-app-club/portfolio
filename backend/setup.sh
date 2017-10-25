cf1e7b6ddf23cde068c758b9cb547f9f3fd36614
maxchehab <maxchehab@gmail.com>
2017-10-25 00:25:31 +0000

    Replaced mysql driver

--- backend/setup.sh
+++ backend/setup.sh
@@ -5,7 +5,7 @@ echo "🤖 - Hello human-friend. Im gonna install some go dependencies for you."
 
 ## Dependencies
 go get github.com/stretchr/testify/assert;
-go get github.com/siddontang/go-mysql/client;
+go get github.com/go-sql-driver/mysql;
 go get github.com/gorilla/mux;
 go get golang.org/x/crypto/bcrypt;
 go get github.com/gorilla/handlers;
