c49b1c42e2fd8138cde1bf2dc50eabf90701a0de
Flaqueeau <econrad@zagmail.gonzaga.edu>
2017-10-26 13:21:14 -0700

    Just mess with everything

--- backend/app.js
+++ backend/app.js
@@ -9,10 +9,6 @@ var handlers = require('./handlers');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -20,7 +16,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
@@ -57,7 +52,14 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send(`
+    <h1> Oh geeze, there was an error. </h1>
+    <br/>
+    <img src="https://media.giphy.com/media/l46Cg6c3B9sTSyPdu/giphy.gif"/>
+    <pre>
+    ${err}
+    </pre>
+  `);
 });
 
 module.exports = app;
