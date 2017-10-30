07434a9ddb7dd0258d501abe069177fdd0abdffa
Max Chehab <maxchehab@gmail.com>
2017-10-30 13:03:16 -0700

    Created frontend user page and implemented author links for Articles

--- frontend/src/components/Article.js
+++ frontend/src/components/Article.js
@@ -1,11 +1,13 @@
 import Markdown from "./Markdown.js";
+import Link from "next/link";
+
 
 const Article = props => {
   return (
     <div>
       <Markdown src={props.body} key={`${props.pageID}-mark`} />
       <p>
-        By <b>{props.author} </b>
+        By <Link href={{ pathname: "/users", query:{userID: props.userID }}}>{props.author}</Link>
       </p>
       <hr key={`${props.pageID}-hr`} />
     </div>
