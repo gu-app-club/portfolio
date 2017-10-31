4fae904e09fd92034adb387b77f12ea42827a764
Max Chehab <maxchehab@gmail.com>
2017-10-31 12:40:26 -0700

    Styled new article as card with box shadow

--- frontend/src/components/Article.js
+++ frontend/src/components/Article.js
@@ -2,14 +2,48 @@ import Markdown from "./Markdown.js";
 import Link from "next/link";
 
 
+
+const articleStyle = {
+  backgroundColor: "#fff",
+  WebkitTransition: "-webkit-box-shadow .25s",
+  transition: "box-shadow .25s, -webkit-box-shadow .25s",
+  borderRadius: "2px",
+  WebkitBoxShadow: "0 2px 2px 0 rgba(0,0,0,0.14), 0 1px 5px 0 rgba(0,0,0,0.12), 0 3px 1px -2px rgba(0,0,0,0.2)",
+  boxShadow: "0 2px 2px 0 rgba(0,0,0,0.14), 0 1px 5px 0 rgba(0,0,0,0.12), 0 3px 1px -2px rgba(0,0,0,0.2)",
+  padding: "24px",
+  marginBottom: "25px",
+  height: "400px",
+};
+
+const markdownStyle = {
+  overflow: "hidden",
+  maxHeight: "75%"
+};
+
+const headerStyle = {
+  maxHeight: "20%"
+};
+
+const readmoreStyle = {
+  position: "relative",
+  bottom: "34%",
+  background: "linear-gradient(to bottom, rgba(255,255,255,0) 0%,rgba(255,255,255,1) 100%)",
+  filter: "progid:DXImageTransform.Microsoft.gradient( startColorstr='#ffffff', endColorstr='#00ffffff',GradientType=0 )",
+  height: "34%"
+};
+
 const Article = props => {
   return (
-    <div>
-      <Markdown src={props.body} key={`${props.pageID}-mark`} />
-      <p>
-        By <Link href={{ pathname: "/u/" + props.userID }}>{props.author}</Link>
-      </p>
-      <hr key={`${props.pageID}-hr`} />
+   
+    <div class="article-wrapper">
+      <div class="header-wrapper">
+        <h1 style={{marginBottom: "0"}}>{props.name}</h1>
+        <p>By <Link href={{ pathname: "/u/" + props.userID }}>{props.author}</Link></p>
+      </div>
+      <div class="markdown-wrapper">
+        <Markdown src={props.body} key={`${props.pageID}-mark`} />
+      </div>
+      <div class="readmore"></div>
     </div>
   );
 };
--- frontend/src/components/ArticleList.js
+++ frontend/src/components/ArticleList.js
@@ -15,9 +15,9 @@ class ArticleList extends React.Component {
       return (
         <div>
           <p>
-            {"We can't find any posts! 😭 If you're a gonzaga student,"}
+            {"We can't find any posts! 😭 If you're a gonzaga student, "}
             <RedirectToLogin href="/new">
-              click here to add one one.
+              click here to add one.
             </RedirectToLogin>
           </p>
         </div>
@@ -30,7 +30,49 @@ class ArticleList extends React.Component {
       </FadeInDown>
     ));
 
-    return <div>{articles}</div>;
+    return( 
+      <div>
+        <style jsx global>
+        {`
+        .article-wrapper { 
+          cursor: pointer;
+          background-color: #fff;
+          -webkit-transition: -webkit-box-shadow .25s;
+          transition: box-shadow .25s, -webkit-box-shadow .25s;
+          border-radius: 2px;
+          -webkit-box-shadow: 0 2px 2px 0 rgba(0,0,0,0.14), 0 1px 5px 0 rgba(0,0,0,0.12), 0 3px 1px -2px rgba(0,0,0,0.2);
+          box-shadow: 0 2px 2px 0 rgba(0,0,0,0.14), 0 1px 5px 0 rgba(0,0,0,0.12), 0 3px 1px -2px rgba(0,0,0,0.2);
+          padding: 24px;
+          margin-bottom: 25px;
+          height: 400px;
+        }
+
+        .article-wrapper:hover{
+          box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 20px 20px rgba(0, 0, 0, 0.22);
+        }
+
+        .markdown-wrapper{
+          overflow: hidden;
+          max-height: 75%;
+        }
+
+        .header-wrapper{
+          max-height: 20%;
+        }
+
+        .readmore{
+          position: relative;
+          bottom: 34%;
+          background: linear-gradient(to bottom, rgba(255,255,255,0) 0%,rgba(255,255,255,1) 100%);
+          filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#ffffff', endColorstr='#00ffffff',GradientType=0 );
+          height: 34%;
+        }
+
+      `}
+        </style>
+        {articles}
+      </div>
+    );
   }
 }
 
