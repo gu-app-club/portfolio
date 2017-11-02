bad81042113f0af4de9461e2627192a6dc4319d1
Max Chehab <maxchehab@gmail.com>
2017-11-02 13:12:13 -0700

    Created footer

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,15 @@
+const Footer = (props) => (
+    <div>
+      <div style={{
+        position: 'absolute',
+        right: '0',
+        bottom: '0',
+        left: '0',
+        padding: '1rem',
+        textAlign: 'center'
+      }} className="footer">{"Made by "} <a href="https://github.com/gu-app-club/portfolio">GU MAD</a>{"."}</div>
+    </div>
+  );
+  
+export default Footer;
+  
